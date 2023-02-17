c659f8199d9bdbffd35bf648daa8edcf30283ab7
kkkovalov <vlad_kovalov@icloud.com>
2023-02-17 13:50:31 -0800

    Added is_empty(), at() and push() functions

--- index.cpp
+++ index.cpp
@@ -3,11 +3,11 @@
 //     - [x]  New raw data array with allocated memory
 //         - can allocate int array under the hood, just not use its features
 //         - start with 16, or if starting number is greater, use power of 2 - 16, 32, 64, 128
-//     - [ ]  size() - number of items
-//     - [ ]  capacity() - number of items it can hold
-//     - [ ]  is_empty()
-//     - [ ]  at(index) - returns item at given index, blows up if index out of bounds
-//     - [ ]  push(item)
+//     - [x]  size() - number of items
+//     - [x]  capacity() - number of items it can hold
+//     - [x]  is_empty()
+//     - [x]  at(index) - returns item at given index, blows up if index out of bounds
+//     - [x]  push(item)
 //     - [ ]  insert(index, item) - inserts item at index, shifts that index’s value and trailing elements to the right
 //     - [ ]  prepend(item) - can use insert above at index 0
 //     - [ ]  pop() - remove from end, return value
@@ -24,33 +24,29 @@
 class vectArr{
     public:
         int size = 0;
-        int capacity = 0;
-        int size(void){
+        int capacity = 16;
+        bool is_empty(void){
+            if(size == 0) return true;
+            else return false;
+        };
+        int at(int* index){
+            // if (*index < 0 || *index > capacity) return 0;
+            return *(pointerArray + *index);
+        }
+        void push(int item){
+            if(size + 1 > capacity){
+                // resize();
+            } else
+            *(pointerArray + size) = item;
+            size++;
+        }
+    private:
+        int * pointerArray = new (std::nothrow) int[capacity];
 
-        }
-        int capacity(void){
-        }
+
 };
 
 int main(void){
-    int * pArr;
-    int cNum = 0;
-    std::cout<<"Enter a number of integers: "<<std::endl;
-    std::cin>>cNum;
-    pArr = new (std::nothrow) int[cNum];
-    if(pArr == nullptr) {
-        std::cout<<"Error: memory could not be allocated";
-        return 0;
-    }
-    for(int i = 0; i < cNum; i++){
-        std::cout<<"Enter a number to add to the vector"<<std::endl;
-        std::cin>>*(pArr + i);
-    }
-    std::cout<<"Full vector"<<std::endl;
-    for(int i = 0; i < cNum; i++){
-        
-        std::cout<<*(pArr + i)<<' ';
-    }
-    delete[] pArr;
-    return 0;
+    vectArr home;
+    
 }
